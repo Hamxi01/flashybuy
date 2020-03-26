a6ea53447a8d6580990c031ebef4f4d511c4e9d2
Mubeen <mubeen33@gmail.com>
2020-03-26 13:51:44 +0500

    sub categories

---
+++ vendor/edit-subcategories.php
@@ -0,0 +1,170 @@
+<?php include('../includes/db.php'); 
+      include('includes/header.php');
+      include('includes/sidebar.php'); 
+
+
+     $sub_cat_id = base64_decode($_GET['id']);
+     if(isset($_POST['form_category'])){
+
+        $id        =    $_POST['sub_cat_id'];
+        $name        =    $_POST['name'];    
+        $meta_title  =    $_POST['meta_title'];
+        $meta_desc   =    $_POST['meta_description'];
+        $category_id   =   addslashes( $_POST['category_id'] );
+        $slug        =   str_replace(" ","-", $name);
+
+        $query = "update sub_categories SET name='".$name."', meta_title='".$meta_title."', meta_description='".$meta_desc."',slug='".$slug."',category_id='".$category_id."' Where sub_cat_id='".$id."'";       
+        
+        if (mysqli_query($con,$query)){
+
+            echo "<script>window.location.assign('subcategories.php');</script>";
+            $msg = "<span>Categories updated successfully...!!</span>";
+        }
+        else{ 
+            header("location:edit-categories.php?id=".$cat_id);
+            $error = "<span>Something went wrong...!!</span>";
+        }
+
+     }
+//  Get Category data bases on cat_id /////
+
+     $sql = mysqli_query($con, "SELECT * From sub_categories WHERE sub_cat_id=$sub_cat_id");
+        $row = mysqli_num_rows($sql);
+        while ($row = mysqli_fetch_array($sql)){
+
+            $sub_cat_id           = $row['sub_cat_id'];
+            $name                 = $row['name'];
+            $meta_title           = $row['meta_title'];     
+            $meta_description     = $row['meta_description'];
+            $category_id          = $row['category_id'];
+
+        }
+$sql = mysqli_query($con, "SELECT * From categories WHERE cat_id=$category_id");
+        $row = mysqli_num_rows($sql);
+    while ($row = mysqli_fetch_array($sql)){
+
+        $cat_name = $row['name'];
+}
+// <!-- End -->
+?>
+
+
+            <!-- Left Sidebar End -->
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->                      
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+                        <!-- Page-Title -->
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="page-title-box">
+                                    <ol class="breadcrumb pull-right">
+                                        <li><a href="#">Minton</a></li>
+                                        <li><a href="#">Forms</a></li>
+                                        <li class="active">Form Validation</li>
+                                    </ol>
+                                    <h4 class="page-title">Add Subcategories</h4>
+                                </div>
+                            </div>
+                        </div>
+ <!-- Start Showing success or warning Msg -->
+<?php
+if (isset($error)) {?>
+    <div class="row">
+        <div class="col-lg-6 col-sm-offset-3">
+            <div class="alert alert-warning">    
+    <?php echo $error; ?>
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
+<?php
+if (isset($msg)) { ?>
+<div class="row">
+    <div class="col-lg-6 col-sm-offset-3">
+        <div class="alert alert-success">    
+    <?php echo $msg; ?>
+
+        </div>
+    </div>
+</div>
+<?php 
+}
+?>
+
+<!-- End Message Alert -->
+                       <div class="row">
+                            <div class="col-lg-6 col-sm-offset-3">
+                                <div class="card-box">
+                                    <h4 class="m-t-0 header-title"><b>SubCategory Information</b></h4>
+                                    <p class="text-muted font-13 m-b-30">
+                                       edit your Subcategories.
+                                    </p>
+
+                                    <form id="form_category"  method="post" action="">
+                                        <input type="hidden" name="sub_cat_id" value="<?=$sub_cat_id?>">
+                                        <div class="form-group">
+                                            <label for="userName"> Name*</label>
+                                            <input type="text" name="name" required parsley-trigger="change"  placeholder="Name" class="form-control" id="userName" value="<?=$name?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Choose Parent Category</label>
+                                            <select class="form-control select2" name="category_id">
+                                                <option  selected value="<?=$category_id?>" ><?=$cat_name?></option>
+                                            <?php
+                                                $sql = mysqli_query($con, "SELECT * From categories");
+                                                $row = mysqli_num_rows($sql);
+                                                while ($row = mysqli_fetch_array($sql)){
+                                                echo "<option value='". $row['cat_id'] ."'>" .$row['name'] ."</option>" ;
+                                            }
+                                            ?>
+                                            
+                                            </select>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label for="passWord2">Meta title</label>
+                                            <input  type="text" name="meta_title" required placeholder="meta title" class="form-control" value="<?=$meta_title?>">
+                                        </div>
+                                        <div class="form-group">
+                                                    <label>Textarea</label>
+                                                   
+                                                    <textarea required class="form-control" name="meta_description"><?=$meta_description?></textarea>
+                                                
+                                        </div>
+
+                                        <div class="form-group text-right m-b-0">
+                                            <button class="btn btn-primary waves-effect waves-light" name="form_category" type="submit">
+                                                Submit
+                                            </button>
+                                            <button type="reset" class="btn btn-default waves-effect waves-light m-l-5">
+                                                Cancel
+                                            </button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                    <!-- end container -->
+
+                </div>
+                <!-- end content -->
+
+
+
+                <!-- FOOTER -->
+                <?php 
+                     include('includes/footer.php');
+                ?>
+                
---
+++ vendor/subcategories.php
@@ -0,0 +1,142 @@
+<?php include('../includes/db.php'); 
+      include('includes/header.php');
+      include('includes/sidebar.php');
+
+    if($_GET['id']){
+
+        $cat_id = base64_decode($_GET['id']);
+        
+        $sql = "DELETE from sub_categories WHERE sub_cat_id = $cat_id";
+        if(mysqli_query($con,$sql)){
+
+            $msg = "<span>Data Deleted successfully...!!</span>";
+        }
+        else{
+
+            $error = "<span>Something went wrong...!!</span>";
+        }
+    }
+?>      
+            <!-- Left Sidebar End --> 
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->                      
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+                        <!-- Page-Title -->
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="page-title-box">
+                                    <ol class="breadcrumb pull-right">
+                                        <li><a href="#">Minton</a></li>
+                                        <li><a href="#">Tables</a></li>
+                                        <li class="active">Categories</li>
+                                    </ol>
+                                    <h4 class="page-title">Categories</h4>
+                                </div>
+                            </div>
+                        </div>
+<!-- Start Message -->
+<?php
+if (isset($error)) {?>
+    <div class="row">
+        <div class="col-lg-6 col-sm-offset-3">
+            <div class="alert alert-warning">    
+    <?php echo $error; ?>
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
+<?php
+if (isset($msg)) { ?>
+<div class="row">
+    <div class="col-lg-6 col-sm-offset-3">
+        <div class="alert alert-success">    
+    <?php echo $msg; ?>
+
+        </div>
+    </div>
+</div>
+<?php 
+}
+?>
+<!-- End Message  -->
+                        <div class="row">
+							
+
+
+                            <div class="col-lg-12">
+								<div class="card-box">
+                                    <div class="col-lg-9">
+                                        <h4 class="m-t-0 header-title"><b>Categories</b></h4><br>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <button class="btn-rounded btn-primary"><a href="add-categories.php" style="color:#fff">Add new Category</a></button>
+                                    </div>    
+                                    <table class="table table-striped m-0">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Banner</th>
+                                                <th>Icon</th>
+                                                <th>Slug</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+<!-- Fetch Categories -->
+<?php
+
+    $sql = mysqli_query($con, "SELECT * From sub_categories");
+    $i = 0;
+    $row = mysqli_num_rows($sql);
+    while ($row = mysqli_fetch_array($sql)){
+        $id = base64_encode($row['sub_cat_id']);
+        $i++;
+?>
+                                            <tr>
+                                                <th scope="row"><?=$i?></th>
+
+                                                <td><?=$row['name']?></td>
+                                                <td><?=$row['slug']?></td>
+                                                <td><?=$row['meta_title']?></td>
+                                                <td><?=$row['meta_description']?></td>
+                                                <td>
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-inverse dropdown-toggle waves-effect waves-light"             data-toggle="dropdown" aria-expanded="false">Actions<span class="caret"></span>
+                                                        </button>
+                                                        <ul class="dropdown-menu" role="menu">
+                                                            <li><a href="edit-subcategories.php?id=<?=$id?>">Edit</a></li>
+                                                            <li><a href="subcategories.php?id=<?=$id?>">Delete</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+<?php } ?>
+<!-- End categories fetch  -->
+                                        </tbody>
+                                    </table>
+
+								</div>
+							</div>
+
+						</div>
+                    </div>
+                    <!-- end container -->
+
+                </div>
+                <!-- end content -->
+
+
+
+                <!-- FOOTER -->
+<?php include('includes/footer.php')?>
