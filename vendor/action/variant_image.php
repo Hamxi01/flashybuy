61ec50b40f924db46e3bf875e1311fcf9c70594c
Mubeen <mubeen33@gmail.com>
2020-04-07 01:39:36 +0500

    Mubeen

--- vendor/action/variant_image.php
+++ vendor/action/variant_image.php
@@ -25,8 +25,32 @@
                             ?>
                            
 <div class="row">
-<div class="col-lg-3">
-                            <div style="   min-height: 150px;
+    <div class="col-lg-3">
+        <div class="fileupload btn btn-primary waves-effect waves-light">
+            <span><i class="ion-upload m-r-5"></i>Image1</span>
+            <input type="file" name="variant_img1[]" class="upload">
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="fileupload btn btn-primary waves-effect waves-light">
+            <span><i class="ion-upload m-r-5"></i>Image2</span>
+            <input type="file" name="variant_img2[]" class="upload">
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="fileupload btn btn-primary waves-effect waves-light">
+            <span><i class="ion-upload m-r-5"></i>Image3</span>
+            <input type="file" name="variant_img3[]" class="upload">
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="fileupload btn btn-primary waves-effect waves-light">
+            <span><i class="ion-upload m-r-5"></i>Image4</span>
+            <input type="file" name="variant_img4[]" class="upload">
+        </div>
+    </div>
+</div><br>   
+                            <!-- <div style="   min-height: 150px;
     border: 2px dashed rgba(0, 0, 0, 0.3);
     background: white;
     border-radius: 6px;"> <input name="variant_img[]" type="file" /></div>
@@ -51,7 +75,7 @@
                             </div>
                             </div>
                             <br>
-                            <br>
+                            <br> -->
                             <?php
                        }
                 }
