93e4158e8e84f03e46fcef489d7ca2f6f1b8364d
muhmmadrameez <rameezyousuf135@gmail.com>
2020-04-08 22:17:40 -0700

    New Template Edeting

---
+++ new_seller/include/nav.php
@@ -0,0 +1,44 @@
+ <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html"> <img alt="image" src="assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">Flashy Buy</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Bank Details</li>
+            <li class="dropdown active">
+              <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="briefcase"></i><span>Bank Management</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="widget-chart.html">Manage Account</a></li>
+              </ul>
+            </li>
+            <li class="menu-header">Shop Management</li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="copy"></i><span>Shop Management</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="alert.html">Manage Shop</a></li>
+              </ul>
+            </li>
+            <li class="menu-header">Profile</li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="layout"></i><span>Manage Profile</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="basic-form.html">View Profile</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="grid"></i><span>Front End Management</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="basic-table.html">Crousel</a></li>
+                <li><a class="nav-link" href="advance-table.html">Banner</a></li>
+                <li><a class="nav-link" href="datatables.html">Social Media Integration</a></li>
+              </ul>
+            </li>
+          </ul>
+        </aside>
+      </div>
