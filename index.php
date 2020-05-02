3279914ee2ac6f65c7a7d05f05a3494d2c4fa122
Mubeen <mubeen33@gmail.com>
2020-05-02 19:20:46 +0500

    Mubeen

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+<?php include('includes/db.php'); ?>
 <?php include('includes/head.php'); ?>
+
     <div id="homepage-1">
         <div class="ps-home-banner ps-home-banner--1">
             <div class="ps-container">
