906918fa93be6ad9a687ecf18217f8a2c0aff473
Hamxi01 <54498227+Hamxi01@users.noreply.github.com>
2020-05-27 17:02:22 +0500

    hamza

---
+++ includes/deal1.php
@@ -0,0 +1,65 @@
+<?php 
+if (isset($_REQUEST['deal_No'])) {
+    $deal_NO = $_REQUEST['deal_No'];
+    
+}
+$dSql = mysqli_query($con,"SELECT P.*,VPD.v_p_id,VPD.deal_price,VPD.start_date,VPD.end_date,VPD.deal_quantity,VPD.market_price , VPD.deal_NO ,VPD.v_p_d_id,VP.quantity 
+                    FROM vendor_product_deals AS VPD
+                    INNER JOIN vendor_product as VP ON VP.id= VPD.v_p_id
+                    INNER JOIN products AS P ON VPD.product_id = P.product_id 
+                    WHERE   1 = 1  
+                            AND VP.active = 'Y'
+                            AND VP.price > 0
+                            AND deal_NO='10030' group by product_id
+                            ORDER BY v_p_d_id DESC");
+$dRows = mysqli_num_rows($dSql);
+if ($dRows>0) {
+   
+
+while ( $dRes = mysqli_fetch_array($dSql)) {
+    
+    $deal_price = $dRes['deal_price'];
+    $product_id = $dRes['product_id'];
+    $name       = $dRes['name'];
+    $image = $dRes['image1'];
+  if (empty($image)) {
+    
+      $varaintImgQuery = "SELECT main_img from product_variant_images where product_id ='$product_id'";
+      $varaintImgSql   = mysqli_query($con,$varaintImgQuery);
+      while ($productVaraintImg = mysqli_fetch_array($varaintImgSql)) {
+
+        $image = $productVaraintImg['main_img'];
+      }
+  }
+
+
+?>    <div class="ps-product ps-product--inner">
+                            <div class="ps-product__thumbnail"><a href="product.php?id=<?=base64_encode($product_id)?>&name=<?=str_replace(' ','-',$name)?>"><img src="upload/product/200_<?=$image?>" alt=""></a>
+                                <div class="ps-product__badge">-16%</div>
+                                <ul class="ps-product__actions">
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Read More"><i class="icon-bag2"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Quick View"><i class="icon-eye"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Add to Whishlist"><i class="icon-heart"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Compare"><i class="icon-chart-bars"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="ps-product__container">
+                                <p class="ps-product__price sale">R<?=$deal_price?> <del>R<?=$deal_price?> </del><small>18% off</small></p>
+                                <div class="ps-product__content"><a class="ps-product__title" href="product.php?id=<?=base64_encode($product_id)?>&name=<?=str_replace(' ','-',$name)?>"><?=$name?></a>
+                                    <div class="ps-product__rating">
+                                        <select class="ps-rating" data-read-only="true">
+                                            <option value="1">1</option>
+                                            <option value="1">2</option>
+                                            <option value="1">3</option>
+                                            <option value="1">4</option>
+                                            <option value="2">5</option>
+                                        </select><span>01</span>
+                                    </div>
+                                    <!-- <div class="ps-product__progress-bar ps-progress" data-value="94">
+                                        <div class="ps-progress__value"><span></span></div>
+                                        <p>Sold:16</p>
+                                    </div> -->
+                                </div>
+                            </div>
+                        </div>
+                        <?php } } ?>
---
+++ includes/deal1php.php
@@ -0,0 +1,29 @@
+<div class="ps-product ps-product--inner">
+                            <div class="ps-product__thumbnail"><a href="product.php"><img src="img/products/home/1.jpg" alt=""></a>
+                                <div class="ps-product__badge">-16%</div>
+                                <ul class="ps-product__actions">
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Read More"><i class="icon-bag2"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Quick View"><i class="icon-eye"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Add to Whishlist"><i class="icon-heart"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Compare"><i class="icon-chart-bars"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="ps-product__container">
+                                <p class="ps-product__price sale">$567.99 <del>$670.00 </del><small>18% off</small></p>
+                                <div class="ps-product__content"><a class="ps-product__title" href="product.php">Korea Long Sofa Fabric In Blue Navy Color</a>
+                                    <div class="ps-product__rating">
+                                        <select class="ps-rating" data-read-only="true">
+                                            <option value="1">1</option>
+                                            <option value="1">2</option>
+                                            <option value="1">3</option>
+                                            <option value="1">4</option>
+                                            <option value="2">5</option>
+                                        </select><span>01</span>
+                                    </div>
+                                    <div class="ps-product__progress-bar ps-progress" data-value="94">
+                                        <div class="ps-progress__value"><span></span></div>
+                                        <p>Sold:16</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
